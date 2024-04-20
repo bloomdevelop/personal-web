ba34a3798e9eb86a5ce87dcad2d2d13fec2bfbef
Bloom <129090449+Bloom-69@users.noreply.github.com>
2024-04-20 13:37:36 +0000

    fix: improve layout to be responsive.
    Also fixed an issue where the cursor is non-existent

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const banner = document.getElementById("banner");
     const close_banner = document.getElementById("close_banner");
     const banner_closed = JSON.parse(window.localStorage.getItem("banner_closed"))
-    const cursor = document.getElementById("cursor")
     const year = document.getElementById("year")
 
     year.innerHTML = new Date().getFullYear();
@@ -15,13 +14,5 @@ document.addEventListener("DOMContentLoaded", () => {
             banner.style.display = "none";
         })
     }
-
-    document.addEventListener("mouseenter", () => {
-        cursor.style.opacity = 1
-    })
-
-    document.addEventListener("mouseleave", () => {
-        cursor.style.opacity = 0
-    })
 })
 
